b601e97acd5cbc4ffbe047cafe42e03714d1a509
Ефименко Александра Сергеевна <efimenko.2018@stud.nstu.ru>
2023-10-02 21:32:38 +0700

    Саша исправила перевод их hex в bin

--- ЛР №2. Блочные составные шифры. Сеть Фейстеля/Проект Visual Studio/PSZI_lr1_v2/EncoderClass.cs
+++ ЛР №2. Блочные составные шифры. Сеть Фейстеля/Проект Visual Studio/PSZI_lr1_v2/EncoderClass.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Text;
 using System.Linq;
+using PSZI_lr1;
 
 namespace PSZI_lr1_v2
 {
@@ -22,7 +23,6 @@ namespace PSZI_lr1_v2
             string binString = String.Join(String.Empty,
                                hexstring.Select(c => Convert.ToString(Convert.ToInt32(c.ToString(), 16), 2).PadLeft(4, '0')));
             char[] charArray = binString.ToCharArray();
-            Array.Reverse(charArray);
             return new string(charArray);
         }
 
@@ -99,11 +99,20 @@ namespace PSZI_lr1_v2
 
         internal static BitArray BinStringToBitArray(string str)
         {
-            BitArray bits = new BitArray(str.Length);
-            for (int i = 0; i < str.Length; i++)
+            BitArray bits = new BitArray(0);
+            BitArray bitsInByte = new BitArray(8);
+            for (int i = 0; i < str.Length / 8; i++)
             {
-                bits.Set(i, str[i] == '1');
+                for (int j = 0; j < 8; j++)
+                {
+                    bitsInByte.Set(8 - j - 1, str[j] == '1');
+                }
+
+
+                bits = BitArrayFuctions.Append(bits, bitsInByte);
             }
+
+
             return bits;
         }
 
