c205c0a9f7001ebfdc9b2e05d47ed5619c1d0b88
antohachekhov <bortnikovnik@gmail.com>
2023-10-25 19:14:02 +0700

    Добавлена EDE

---
+++ ЛР №4. Режимы работы блочных шифров. Схемы кратного шифрования/Проект Visual Studio/PSZI_lr1_v2/EDE.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSZI_lr1_v2
+{
+    internal class EDE
+    {
+        static BitArray encrypt(BitArray inputText, BitArray key1, BitArray key2, BitArray key3)
+        {
+            EncryptByDES des1 = new EncryptByDES(new GeneratorKey(key1));
+            EncryptByDES des2 = new EncryptByDES(new GeneratorKey(key2));
+            EncryptByDES des3 = new EncryptByDES(new GeneratorKey(key3));
+
+            BitArray tempResult = des1.Encrypte(inputText);
+            tempResult = des2.Decrypte(tempResult);
+            tempResult = des3.Encrypte(tempResult);
+            return tempResult;
+        }
+    }
+}
