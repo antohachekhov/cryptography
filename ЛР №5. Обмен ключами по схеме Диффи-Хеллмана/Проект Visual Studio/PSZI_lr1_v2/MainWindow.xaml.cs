4e41e2fbce249b4deb3d3bcafe09caabfcb0e469
AlexsandraMK <se16052000@mail.ru>
2023-10-29 21:23:10 +0700

    Начало 5лр

---
+++ ЛР №5. Обмен ключами по схеме Диффи-Хеллмана/Проект Visual Studio/PSZI_lr1_v2/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Win32;
+using PSZI_lr1;
+using System.Diagnostics;
+using System.IO;
+using System.Collections;
+using System.Windows.Controls;
+using System.Linq;
+
+namespace PSZI_lr1_v2
+{
+
+
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Program program = null;
+
+        public MainWindow()
+        {
+            program = new Program();
+            InitializeComponent();
+
+        }
+    }
+}
---
+++ ЛР №5. Обмен ключами по схеме Диффи-Хеллмана/Проект Visual Studio/PSZI_lr1_v2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Collections;
+using System.Diagnostics;
+
+
+namespace PSZI_lr1_v2
+{
+    public static class BitArrayFunctions
+    {
+        public static BitArray Append(this BitArray current, BitArray after)
+        {
+            var bools = new bool[current.Count + after.Count];
+            current.CopyTo(bools, 0);
+            after.CopyTo(bools, current.Count);
+            return new BitArray(bools);
+        }
+
+        public static BitArray ReverseAll(this BitArray current)
+        {
+            var bools = new bool[current.Count];
+            for (int i = 0; i < current.Count; i++)
+            {
+                bools[i] = current.Get(current.Count - i - 1);
+            }
+            return new BitArray(bools);
+        }
+
+        public static BitArray ReverseOnlyValuesInBytes(this BitArray current)
+        {
+            BitArray bitArray = new BitArray(0);
+            for (int i = 0; i < current.Count / 8; i++)
+            {
+                bool[] bools = new bool[8];
+                for (int j = 0; j < 8; j++)
+                {
+                    bools[j] = current.Get((i + 1) * 8 - j - 1);
+                }
+                BitArray bitArrayInByte = new BitArray(bools);
+                bitArray = BitArrayFunctions.Append(bitArray, bitArrayInByte);
+            }
+            return bitArray;
+        }
+
+        public static int CountXor1(BitArray first, BitArray second)
+        {
+            BitArray xor = new BitArray(first);
+            xor.Xor(second);
+
+            int count = 0;
+            for(int i =0; i < xor.Length; i++)
+            {
+                if (xor[i] == true)
+                    count++;
+            }
+
+            return count;
+        }
+    }
+
+    public class Program
+    {
+
+        // Количество бит 
+        public int n;
+
+
+        
+    }
+}
